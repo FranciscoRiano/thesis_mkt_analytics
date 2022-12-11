d633de6aa46c0626560a734d74581b202244cffb
FranciscoRiano <friano_95@hotmail.com>
2022-12-11 17:51:04 +0100

    Chapter 4 created; new code has been added in the r file, individual regression and t-tests

--- r_analysis.R
+++ r_analysis.R
@@ -1,58 +1,85 @@
-library(haven)      # Read and write Stata, SPSS, or SAS data file 
-library(lavaan)     # LAtent VAriable ANalysis    # Diagrams of models estimated with lavaan
 library(ltm)
 library(dplyr)
 library(tidyverse)
 library(car)
 library(ggpubr)
-library(mediation)
+library(semPlot)
 
 data_thesis <- read.csv("C:/Users/frian/thesis_francisco_riano_ma/final_data.csv")
 #data_thesis <- read.csv("C:/Users/frian/OneDrive/Documentos - copia/tilburg/Lessons/test_1.csv")
 head(data_thesis$condition)
 head(data_thesis$type2)
 head(data_thesis$items_d2)
-dim(data_thesis)
+summary(data_thesis)
 
 #data cleaning and processing
-
 #step1
 
 data_thesis %>% 
-  group_by(items_d, type) %>% 
-  summarize(wtp_avg = mean(wtp), wtp = sum(wtp), po_avg = mean(PO), po = sum(PO), amt = n())
+  group_by(condition) %>% 
+  summarize( amt = n(), wtp_avg = mean(wtp), sd_wtp = sd(wtp), po_avg = mean(PO), po_sd = sd(PO))
 
 head(data_thesis$items_d)
 
 data_thesis <- data_thesis %>% 
-  mutate(items_d2 = case_when(items_d == "high" ~ 1,
-                              TRUE ~ 0))
+  mutate(items_d2 = case_when(items_d == "high" ~ 0,
+                              TRUE ~ 1))
 
 
 data_thesis <- data_thesis %>% 
-  mutate(type_2 = case_when(type == "hedonic" ~ 1,
-                              TRUE ~ 0))
+  mutate(type_2 = case_when(type == "hedonic" ~ 0,
+                              TRUE ~ 1))
 
 data_thesis <- data_thesis %>% 
-  mutate(gender_ = case_when(Q91 == "Non-binary / third gender" ~ 1,
-                             Q91 == "Male" ~ 2,
-                             Q91 == "Female" ~ 3))
-
+  mutate(gender_ = case_when(Q91 == "Male" ~ 0,
+                             Q91 == "Female" ~ 1))
 
 
 
 data_thesis2 <- data_thesis %>% 
-  dplyr::select(wtp:type_2)
-
-data_thesis2 <- data_thesis2 %>% 
-  mutate(wtp_ = as.numeric(wtp))
-
-data_thesis2 <- data_thesis2 %>% 
+  dplyr::select(wtp:gender_) %>% 
+  mutate(wtp_ = as.numeric(wtp)) %>% 
   mutate(favorite_means_tr = Q93) %>% 
   mutate(knowledge_cars = Q112_1) %>% 
-  mutate(gender = Q91) %>% 
   mutate(age_rg = Q92) %>% 
-  dplyr::select(-Q93, -Q112_1, -Q91, -Q92, wtp)
+  dplyr::select(-Q93, -Q112_1, -Q91, -Q92, -wtp,-items) %>% 
+  dplyr::select(-Q62_1:-Q62_4_)
+
+
+#trasnformation of the covariates
+
+data_thesis2 <- data_thesis2 %>% 
+  mutate(favorite_means_tr_moc = case_when(favorite_means_tr == "My own car" ~ 1,
+                                           TRUE ~ 0)) %>% 
+  mutate(favorite_means_tr_by = case_when(favorite_means_tr == "Bike" ~ 1,
+                                           TRUE ~ 0)) %>%
+  mutate(favorite_means_tr_Ot = case_when(favorite_means_tr == "Other" ~ 1,
+                                          TRUE ~ 0)) %>%
+  mutate(favorite_means_tr_Pt = case_when(favorite_means_tr == "Public transport" ~ 1,
+                                          TRUE ~ 0)) %>%
+  mutate(favorite_means_tr_rc = case_when(favorite_means_tr == "Rented or leased car" ~ 1,
+                                          TRUE ~ 0)) %>%
+  mutate(favorite_means_tr_sm = case_when(favorite_means_tr == "Scooter or motorbike" ~ 1,
+                                          TRUE ~ 0)) %>% 
+  mutate(age_rg_25 = case_when(age_rg == "from 21 years old to 25 years old" ~ 1,
+                               TRUE ~ 0)) %>% 
+  mutate(age_rg_30 = case_when(age_rg == "from 26 years old to 30 years old" ~ 1,
+                               TRUE ~ 0)) %>% 
+  mutate(age_rg_35 = case_when(age_rg == "from 31 years old to 35 years old" ~ 1,
+                               TRUE ~ 0)) %>%
+  mutate(age_rg_40 = case_when(age_rg == "from 36 years old to 40 years old" ~ 1,
+                               TRUE ~ 0)) %>%
+  mutate(age_rg_45 = case_when(age_rg == "from 41 years old to 45 years old" ~ 1,
+                               TRUE ~ 0)) %>% 
+  mutate(age_rg_50 = case_when(age_rg == "from 46 years old to 50 years old" ~ 1,
+                               TRUE ~ 0))
+  
+  
+  
+data_thesis %>% 
+  group_by(items_d2) %>% 
+  summarize( amt = n(), wtp_avg = mean(wtp), sd_wtp = sd(wtp), po_avg = mean(PO), po_sd = sd(PO))
+
 
 
 
@@ -63,28 +90,64 @@ sum(is.na(data_thesis2))
 
 
 process(data = data_thesis2, y = "wtp_", x = "items_d2" , m = "PO", w = "type_2", model = 8, 
-        cov = c("favorite_means_tr", "knowledge_cars", "gender", "age_rg"), center = 2, moments = 1, modelbt = 1, boot = 1000, seed = 19421)
+        cov = c("favorite_means_tr", "knowledge_cars", "gender_"), center = 2, moments = 1, modelbt = 1, boot = 1000, seed = 19421)
  
-View(mtcars)
-process(mtcars, y = "mpg", x = "hp", m = "wt", model = 4)                                                                                           
-    
+                                                                                      
+process(data = data_thesis2, y = "wtp_", x = "items_d2" , m = "PO", w = "type_2", model = 8, 
+        cov = c("knowledge_cars", "gender_","favorite_means_tr_by", "favorite_means_tr_Ot",
+                "favorite_means_tr_Pt", "favorite_means_tr_rc", "favorite_means_tr_sm", "age_rg_30",
+                "age_rg_35", "age_rg_40", "age_rg_45", "age_rg_50"), center = 2, moments = 1, modelbt = 1, boot = 1000, seed = 19421)
+
+process(data = data_thesis2, y = "wtp_", x = "items_d2" , m = "PO", w = "type_2", model = 8, 
+        cov = c("knowledge_cars", "gender_","favorite_means_tr_by", "favorite_means_tr_Ot",
+                "favorite_means_tr_Pt", "favorite_means_tr_rc", "favorite_means_tr_sm"), center = 2, moments = 1, modelbt = 1, boot = 1000, seed = 19421)
 
 
-class(mtcars$mpg)
-class(mtcars$hp)
-class(mtcars$wt)
-     
-class(data_thesis$wtp_)
-class(data_thesis$PO)
+
+#Individual regressions and t-test
+movies_lm2_0 <- lm(wtp_~items_d2*type_2+gender_+knowledge_cars+favorite_means_tr_by+favorite_means_tr_Ot+favorite_means_tr_Pt+
+                     favorite_means_tr_rc+favorite_means_tr_sm+ age_rg_30+age_rg_35+age_rg_40+age_rg_45+age_rg_50 , data_thesis2);
+summary(movies_lm2_0)
+Anova(movies_lm2_0, type=3)
+
+help("Anova")
+movies_lm2_1 <- lm(PO~items_d2*type_2+gender_+knowledge_cars+favorite_means_tr_by+favorite_means_tr_Ot+favorite_means_tr_Pt+
+                     favorite_means_tr_rc+favorite_means_tr_sm+ age_rg_30+age_rg_35+age_rg_40+age_rg_45+age_rg_50  , data_thesis2);
+summary(movies_lm2_1)
+Anova(movies_lm2_1, type=3)
+
+
+  #t-test for wtp
+t.test(wtp_ ~ items_d2, alternative = "greater", data = data_thesis2, var.equal=TRUE)
+t.test(wtp_ ~ items_d2, alternative = "less", data = data_thesis2, var.equal=TRUE)
+t.test(wtp_ ~ items_d2, alternative = "two.sided", data = data_thesis2, var.equal=TRUE)
+
+t.test(wtp_ ~ type_2, alternative = "less", data = data_thesis2, var.equal=TRUE)
+t.test(wtp_ ~ type_2, alternative = "greater", data = data_thesis2, var.equal=TRUE)
+t.test(wtp_ ~ type_2, alternative = "two.sided", data = data_thesis2, var.equal=TRUE)
+  #t-tests fo PO
+
+t.test(PO ~ items_d2, alternative = "greater", data = data_thesis2, var.equal=TRUE)
+t.test(PO ~ items_d2, alternative = "less", data = data_thesis2, var.equal=TRUE)
+t.test(PO ~ items_d2, alternative = "two.sided", data = data_thesis2, var.equal=TRUE)
+#high es 1 low es 0
+t.test(PO ~ type_2, alternative = "less", data = data_thesis2, var.equal=TRUE)
+t.test(PO ~ type_2, alternative = "greater", data = data_thesis2, var.equal=TRUE)
+t.test(PO ~ type_2, alternative = "two.sided", data = data_thesis2, var.equal=TRUE)
+help("t.test")
+
+
+class(data_thesis2$wtp_)
+class(data_thesis2$PO)
 class(data_thesis2$type_2)
 class(data_thesis2$items_d2)
 
-summarize(data_thesis)
+summary(data_thesis2)
 
 
 data_thesis2 %>% 
   group_by(condition) %>% 
-  summarize(dw = mean(PO), n = n(), mad = mean(wtp))
+  summarize(dw = mean(PO), n = n(), mad = mean(wtp_))
 
 
 #visualizations
@@ -132,31 +195,39 @@ boxplot(PO ~ condition,
         border = "black")
 
 
-
+  #Bar visualizations
 data_thesis %>% 
   group_by(items_d, type) %>% 
   summarize(wtp_avg = mean(wtp)) %>% 
   ggplot(aes(x = items_d, y = wtp_avg, fill = type))+
-  geom_col(position = "dodge")
+  geom_col(position = "dodge")+
+  labs(x = "Level of Customization", y = "Willingness to pay", fill = "Type of features")+
+  ggtitle("Willingness to pay by level of customization ")+
+  theme(panel.background = element_blank())
+
+
 
 data_thesis %>% 
   group_by(items_d, type) %>% 
   summarize(po_avg = mean(PO)) %>% 
   ggplot(aes(x = items_d, y = po_avg, fill = type))+
-  geom_col(position = "dodge")
-
+  geom_col(position = "dodge")+
+  labs(x = "Level of Customization", y = "Psychological Ownership", fill = "Type of features")+
+  ggtitle("Psychological ownership by level of customization ")+
+  theme(panel.background = element_blank())
 
 
 #randomization
 
 
-evaltot_aov1 <- lm(gender_ ~ items ,data_thesis)
+evaltot_aov1 <- lm(gender_ ~ conditions ,data_thesis)
 Anova(evaltot_aov1, type=3)
-
+evaltot_aov2 <- lm(wtp ~ gender_ ,data_thesis)
+summary(evaltot_aov2)
+Anova(evaltot_aov2, type=3)
 
 #Cronbach's alpha
-data_thesis3 <- data_thesis2 %>% 
-  dplyr::select(Q62_1,Q62_2,Q62_3,Q62_4)
+
 
 data_thesis3 <- data_thesis2 %>% 
   dplyr::select(Q62_1_,Q62_2_,Q62_3_,Q62_4_)
@@ -172,5 +243,60 @@ leveneTest(PO ~ items_d, data_thesis2, center=mean)
 shapiro.test(data_thesis2$wtp)
 shapiro.test(data_thesis2$PO)
   
+
+#correlations
+
+data_thesis4 <- data_thesis2 %>% 
+  dplyr::select(-Q62_1,-Q62_2,-Q62_3,-Q62_4,-items_d,-items,-type,-condition,) %>% 
+  mutate(favorite_means_tr_ = case_when(favorite_means_tr == "My own car" ~ 1,
+                                        favorite_means_tr == "Rented or leased car" ~ 2,
+                                        favorite_means_tr == "Bike" ~ 3,
+                                        favorite_means_tr == "Scooter or motorbike" ~ 4,
+                                        favorite_means_tr == "Public transport" ~ 5,
+                                        favorite_means_tr == "Other" ~ 6)) %>% 
+  dplyr::select(-favorite_means_tr) %>% 
+  mutate(age_rg_ = case_when(age_rg == "from 21 years old to 25 years old" ~ 1,
+                             age_rg == "from 26 years old to 30 years old" ~ 2,
+                             age_rg == "from 31 years old to 35 years old" ~ 3,
+                             age_rg == "from 36 years old to 40 years old" ~ 4,
+                             age_rg == "from 41 years old to 45 years old" ~ 5,
+                             age_rg == "from 45 years old to 50 years old" ~ 6,
+                             age_rg == "51 years old or older" ~ 7)) %>% 
+  dplyr::select(-age_rg) %>% 
+  dplyr::select(-gender, -Q62_1_,-Q62_2_,-Q62_3_,-Q62_4_) 
   
   
+  
+
+
+
+
+data_thesis4.cor = cor(data_thesis4)
+  
+data_thesis4.cor = cor(data_thesis4, method = c("spearman"))
+library(corrplot)
+
+corrplot(data_thesis4.cor)
+lapply(split(data_thesis4[,1:5],data_thesis4$gender_),cor)
+
+
+cor.test(data_thesis2$wtp, data_thesis2$type_2)
+cor.test(data_thesis2$PO, data_thesis2$type_2)
+cor.test(data_thesis2$PO, data_thesis2$wtp_)
+cor.test(data_thesis2$PO, data_thesis2$knowledge_cars)
+cor.test(data_thesis2$wtp, data_thesis2$knowledge_cars)
+cor.test(data_thesis4$PO, data_thesis4$gender_)
+
+
+
+
+
+library(JSmediation)
+
+moderated_mediation_fit <- 
+  mdt_moderated(data = data_thesis2,
+                IV   = items_d2,
+                DV   = wtp_, 
+                M    = PO,
+                Mod  = type_2)
+moderated_mediation_fit
